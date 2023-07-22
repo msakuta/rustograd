bbbf991ecc178741a1aa1e41ce0d01b5908890d8
msakuta <masahiro.sakuta@gmail.com>
2023-07-22 16:46:53 +0900

    Add subtraction and division operators

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,12 @@
-use std::ops::{Add, Mul};
+use std::ops::{Add, Div, Mul, Sub};
 
 #[derive(Clone, Debug)]
 enum TermInt<'a> {
     Value(f64),
     Add(&'a Term<'a>, &'a Term<'a>),
+    Sub(&'a Term<'a>, &'a Term<'a>),
     Mul(&'a Term<'a>, &'a Term<'a>),
+    Div(&'a Term<'a>, &'a Term<'a>),
 }
 
 type Term<'a> = Box<TermInt<'a>>;
@@ -16,10 +18,24 @@ impl<'a> Add for &'a Term<'a> {
     }
 }
 
+impl<'a> Sub for &'a Term<'a> {
+    type Output = Term<'a>;
+    fn sub(self, rhs: Self) -> Self::Output {
+        Box::new(TermInt::Sub(self, rhs))
+    }
+}
+
 impl<'a> Mul for &'a Term<'a> {
     type Output = Term<'a>;
     fn mul(self, rhs: Self) -> Self::Output {
         Box::new(TermInt::Mul(self, rhs))
+    }
+}
+
+impl<'a> Div for &'a Term<'a> {
+    type Output = Term<'a>;
+    fn div(self, rhs: Self) -> Self::Output {
+        Box::new(TermInt::Div(self, rhs))
     }
 }
 
@@ -34,10 +50,20 @@ impl<'a> TermInt<'a> {
             match self {
                 Self::Value(_) => 0.,
                 Self::Add(lhs, rhs) => lhs.derive(var) + rhs.derive(var),
+                Self::Sub(lhs, rhs) => lhs.derive(var) - rhs.derive(var),
                 Self::Mul(lhs, rhs) => {
                     let dlhs = lhs.derive(var);
                     let drhs = rhs.derive(var);
                     dlhs * rhs.eval() + lhs.eval() * drhs
+                }
+                Self::Div(lhs, rhs) => {
+                    let dlhs = lhs.derive(var);
+                    let drhs = rhs.derive(var);
+                    if drhs == 0. {
+                        dlhs / rhs.eval()
+                    } else {
+                        dlhs / rhs.eval() + lhs.eval() / drhs
+                    }
                 }
             }
         }
@@ -47,7 +73,9 @@ impl<'a> TermInt<'a> {
         match self {
             Self::Value(val) => *val,
             Self::Add(lhs, rhs) => lhs.eval() + rhs.eval(),
+            Self::Sub(lhs, rhs) => lhs.eval() - rhs.eval(),
             Self::Mul(lhs, rhs) => lhs.eval() * rhs.eval(),
+            Self::Div(lhs, rhs) => lhs.eval() / rhs.eval(),
         }
     }
 }
@@ -68,4 +96,9 @@ fn main() {
     println!("d((a + b) * c) / db = {}", abc_b);
     let abc_c = abc.derive(&c);
     println!("d((a + b) * c) / dc = {}", abc_c);
+
+    let d = TermInt::new(2.);
+    let abcd = &abc / &d;
+    let abcd_c = abcd.derive(&c);
+    println!("d((a + b) * c / d) / dc = {}", abcd_c);
 }
