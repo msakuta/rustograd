a341891cab192249b1024c9dcaadc6a9f895a654
msakuta <masahiro.sakuta@gmail.com>
2023-08-14 01:22:30 +0900

    Add examples

---
+++ examples/tape_gen_graph_mul.rs
@@ -0,0 +1,20 @@
+use rustograd::Tape;
+
+fn main() {
+    let tape = Tape::new();
+    let a = tape.term("a", 123.);
+    let b = tape.term("b", 321.);
+    // let c = tape.term("b", 42.);
+    let aba = (a - b) * a;
+    let daba = aba.gen_graph(&a).unwrap();
+    aba.eval();
+    aba.backprop().unwrap();
+    daba.eval_noclear();
+
+    // assert!(daba.gen_graph(&a).is_none());
+
+    daba.dot_builder()
+        .show_values(true)
+        .dot(&mut std::io::stdout())
+        .unwrap();
+}
