6932989498625b8ab12939d7ab2ecdaaa7ca5b93
msakuta <masahiro.sakuta@gmail.com>
2023-08-18 00:08:51 +0900

    Add a polynomial example to gen_graph

---
+++ examples/tape_gen_graph_poly.rs
@@ -0,0 +1,76 @@
+//! gen_graph example with polynomials.
+use std::io::Write;
+
+use rustograd::{
+    tape::{add_mul, add_unary_fn, add_value, TapeNode},
+    Tape, UnaryFn,
+};
+
+struct PolynomialFn(i32);
+
+impl UnaryFn<f64> for PolynomialFn {
+    fn name(&self) -> String {
+        format!("(^{})", self.0)
+    }
+
+    fn f(&self, data: f64) -> f64 {
+        data.powi(self.0)
+    }
+
+    fn grad(&self, data: f64) -> f64 {
+        self.0 as f64 * data.powi(self.0 - 1)
+    }
+
+    fn gen_graph(
+        &self,
+        nodes: &mut Vec<TapeNode<f64>>,
+        _idx: u32,
+        input: u32,
+        _derived: u32,
+    ) -> Option<u32> {
+        if self.0 == 0 {
+            None
+        } else {
+            let poly = add_unary_fn(nodes, Box::new(Self(self.0 - 1)), input);
+            let factor = add_value(nodes, self.0 as f64);
+            Some(add_mul(nodes, factor, poly))
+        }
+    }
+}
+
+fn main() {
+    let tape = Tape::new();
+    let a = tape.term("a", 1.23);
+    let sin_a = (a).apply_t(Box::new(PolynomialFn(3)));
+
+    let mut csv = std::io::BufWriter::new(std::fs::File::create("data.csv").unwrap());
+    let mut derivatives = vec![sin_a];
+    let mut next = sin_a;
+    write!(csv, "x, $x^3)$, ").unwrap();
+    for i in 1..4 {
+        let Some(next_i) = next.gen_graph(&a) else { break };
+        next = next_i;
+        derivatives.push(next);
+        write!(csv, "$(d^{i} x^3)/(d x^{i})$, ").unwrap();
+    }
+    writeln!(csv, "").unwrap();
+
+    for i in -50..50 {
+        let x = i as f64 * 0.1;
+        a.set(x).unwrap();
+        write!(csv, "{x}, ").unwrap();
+        for d in &derivatives {
+            write!(csv, "{}, ", d.eval()).unwrap();
+        }
+        writeln!(csv, "").unwrap();
+    }
+
+    let mut writer = std::io::BufWriter::new(std::fs::File::create("graph.dot").unwrap());
+    derivatives
+        .last()
+        .unwrap()
+        .dot_builder()
+        .show_values(true)
+        .dot(&mut writer)
+        .unwrap();
+}
--- src/tape.rs
+++ src/tape.rs
@@ -422,7 +422,7 @@ pub fn add_value<T: Tensor>(nodes: &mut Vec<TapeNode<T>>, val: T) -> u32 {
     add_node(nodes, name, TapeValue::Value(val))
 }
 
-fn add_add<T: Tensor>(nodes: &mut Vec<TapeNode<T>>, lhs: u32, rhs: u32) -> u32 {
+pub fn add_add<T: Tensor>(nodes: &mut Vec<TapeNode<T>>, lhs: u32, rhs: u32) -> u32 {
     let name = format!(
         "{} + {}",
         nodes[lhs as usize].name, nodes[rhs as usize].name
